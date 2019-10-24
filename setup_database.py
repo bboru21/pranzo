b617de916581ec37dd6ba6ce4a1ba2513050a7ce
Bryan Hadro <bryan.e.hadro@gmail.com>
2019-10-24 09:26:22 -0400

    add initial database setup files

---
+++ setup_database.py
@@ -0,0 +1,17 @@
+# -*- coding: UTF-8 -*-
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+class Vendors(Base):
+
+    __tablename__ = 'vendors'
+    id = Column(Integer, primary_key=True)
+    site_permit = Column(String, unique=True)
+    name = Column(String)
+    alias = Column(String)
+
+engine = create_engine('sqlite:///db/pranzodb.db', echo=True)
+Base.metadata.create_all(engine)
