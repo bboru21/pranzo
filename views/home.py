e20d2c7056c02d4509f737a26f7c943e2658f2b7
Bryan Hadro <bryan.e.hadro@gmail.com>
2019-11-18 09:26:49 -0500

    add post database update

--- views/home.py
+++ views/home.py
@@ -6,12 +6,38 @@ from sqlalchemy.orm import sessionmaker
 from models import Vendor
 from simple_settings import settings
 
+from contextlib import contextmanager
+
+@contextmanager
+def session_scope():
+    """Provide a transactional scope around a series of operations."""
+    engine = create_engine(settings.DATABASES['ENGINE'])
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    try:
+        yield session
+        session.commit()
+    except:
+        session.rollback()
+        raise
+    finally:
+        session.close()
+
 @view_config(route_name='update_alias', renderer='json')
 def update_alias(request):
 
-    print(request.POST)
+    if request.POST:
+        id = request.POST.get('id')
+        alias = request.POST.get('alias')
+        if id and alias:
+            with session_scope() as session:
+                vendor = session.query(Vendor).filter_by(id=id).first()
+                vendor.alias = alias
+                session.commit()
 
-    return { 'success': True }
+                return { 'success': True }
+
+    return { 'success': False }
 
 @view_config(route_name='home', renderer='../templates/vendors.mako')
 def home_page(request):
